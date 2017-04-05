fbec8e16dbc2a3e261ae425afd1d1ae23465eee3
Luke Korth <luke@Lukes-MacBook-Pro.local>
2017-04-05 13:56:29 -0500

    Made some formatting changes.

--- chad.rb
+++ chad.rb
@@ -1,10 +1,14 @@
 require "mechanize"
 
-most_recent_review = "Shawarma Express"
+most_recent_review = "Some Business"
+slack_channel      = "Define the Slack channel to post to here. Format is #channel"
+slack_user         = "Define the Slack user to use."
+slack_emoji        = "Define the Slack emoji for the user's avatar."
+slack_url          = "Define the Slack hook URL to POST the request to."
 
 #Get name of most recent review.
 agent = Mechanize.new
-page  = agent.get("https://www.yelp.com/user_details_reviews_self?userid=0_3KQG5DnBAxG6O8CC1t3A")
+page  = agent.get("Yelp URL goes here.")
 search = page.search('//div[@class="media-story"]').first
 string = search.elements.first.text
 string.gsub!("\n", "")
@@ -18,9 +22,9 @@ if string != most_recent_review
 	emoji  = ":star:"
 	post_string = "Here's my #{emoji * rating} review for #{string}!"
 	#post to slack
-	`curl -X POST --data-urlencode 'payload={"channel": "#ji-chi", "username": "chadbot", "text": "#{post_array.sample}", "icon_emoji": ":chad:"}' https://hooks.slack.com/services/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`
+	`curl -X POST --data-urlencode 'payload={"channel": "#{slack_channel}", "username": "#{slack_user}", "text": "#{post_string}", "icon_emoji": "#{slack_emoji}"}' #{slack_url}`
 	#change most recent review in this file
-	file_name = "/Users/luke/Code_Personal/chad.rb"
+	file_name = "/path/to/this/file"
 	text = File.read(file_name)
 	a = text.match /most_recent_review\s=\s(?<review>[^\n]+)/
 	old_string = a[:review]
